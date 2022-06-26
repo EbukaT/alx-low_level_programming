655767d0caf9283e3cf20a858e082600efe43c70
EbukaT <timothyugwuanyi9@gmail.com>
2022-06-26 03:56:51 -0700

    Static lib

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,31 +1,26 @@
-#include "main.h"
-
+#include"main.h"
 /**
- * _strstr - finds the first occurrence of the substring.
- * needle in the string haystack.
- * @haystack: entire string.
- * @needle: substring.
- * Return: pointer to the beginning of located substring or
- * NULL if the substring is not found.
-*/
+ * _strstr - locating a substring
+ * @needle: occurance to find
+ * @haystack: string to search
+ * Return: pointer
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	char *bhaystack;
-	char *pneedle;
 
-	while (*haystack != '\0')
+	while (*haystack)
 	{
-		bhaystack = haystack;
-		pneedle = needle;
+		char *src = haystack;
+		char *sub = needle;
 
-		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		while (*haystack && *sub && *haystack == *sub)
 		{
 			haystack++;
-			pneedle++;
+			sub++;
 		}
-		if (!*pneedle)
-			return (bhaystack);
-		haystack = bhaystack + 1;
+		if (!*sub)
+			return (src);
+		haystack = src + 1;
 	}
 	return (0);
 }
